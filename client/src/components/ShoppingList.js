71b86c4ddf934f3299e4293e0b8a62f235cdefcb
VethursanJ <Vethursan.Jegatheeswaran@ec.gc.ca>
2018-12-04 18:59:43 -0500

    added list with add and remove actions

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,6 +1,6 @@
-import React, { component } from "react";
+import React, { Component } from "react";
 import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
-import { CSSTransition, TransitionGroup } from "react-transiton-group";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import uuid from "uuid";
 
 class ShoppingList extends Component {
@@ -15,12 +15,46 @@ class ShoppingList extends Component {
 
   render() {
     const { items } = this.state;
-    return(
-        <Container>
-            <Button
-            color="dark"
-            style={{marginBottom:'2rem' '6:00'}}>Add Item</Button>
-        </Container>
+    return (
+      <Container>
+        <Button
+          color="dark"
+          style={{ marginBottom: "2rem" }}
+          onClick={() => {
+            const name = prompt("Enter Item");
+            if (name) {
+              this.setState({
+                items: [...this.state.items, { id: uuid(), name }]
+              });
+            }
+          }}
+        >
+          Add Item
+        </Button>
+        <ListGroup>
+          <TransitionGroup className="shopping_list">
+            {items.map(({ id, name }) => (
+              <CSSTransition key={id} timeout={500} classNames="fade">
+                <ListGroupItem>
+                  <Button
+                    className="remove-btn"
+                    color="danger"
+                    size="sm"
+                    onClick={() => {
+                      this.setState({
+                        items: this.state.items.filter(item => item.id !== id)
+                      });
+                    }}
+                  >
+                    &times;
+                  </Button>
+                  {name}
+                </ListGroupItem>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </ListGroup>
+      </Container>
     );
   }
 }
